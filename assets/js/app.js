1b9c242fc5c144c80fabd4b684b26a8456ca3c32
Roberto <btofpach@gmail.com>
2023-10-27 20:16:02 -0300

    mostrar peliculas API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,30 @@
 const api_key = "api_key=78d991126ffe36d8e5a86072b85d5d2c";
 const url = "https://api.themoviedb.org/3/movie/popular?api_key=78d991126ffe36d8e5a86072b85d5d2c&language=es-MX";
 
-const peliculasPopulares = async() => {
+const mostrarPeliculas = async(cantidadPeliculas) => {
     
     const respuesta = await fetch(url);
-
-
     const datos = await respuesta.json();
 
-    let peliculas = '';
-    datos.results.forEach(pelicula=> {
+
+    let peliculas = "";
+    let poster = "";
+    let titulo = "";
+
+    for (let i = 0; i < cantidadPeliculas; i++) {
+        let pelicula = datos.results[i];
         peliculas += `
             <div class="pelicula">
                 <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
                 <h3 class="titulo">${pelicula.title}</h3>
-            </div>`;
-        });
-        document.getElementById('contenedor').innerHTML = peliculas;
+            </div>
+            `;
+        };
+
+
+        document.getElementById("contenedor").innerHTML = peliculas;
 
     };
 
 
-peliculasPopulares();
+mostrarPeliculas(3);
