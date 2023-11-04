c3213ef85d99035381e3a957fb3256b6624c1fe3
Roberto <btofpach@gmail.com>
2023-11-04 19:54:36 -0300

    mas cambios API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,65 @@
-const api_key = "api_key=78d991126ffe36d8e5a86072b85d5d2c";
-
-
+// const api_key = "api_key=78d991126ffe36d8e5a86072b85d5d2c";
 // cantidadPeliculas: cantidad de peliculas a mostrar, 1 a 20.
 // tipo puede ser: "now_playing", "upcoming", "popular" o "top_rated"
 // id; id del contenedor
 
-
-const mostrarPeliculas = async(cantidadPeliculas, tipo, id) => {
-
-    const url = "https://api.themoviedb.org/3/movie/" + tipo + "?api_key=78d991126ffe36d8e5a86072b85d5d2c&language=es-MX";
+const mostrarPeliculas = async() => {
+    const url = "https://api.themoviedb.org/3/movie/" + "upcoming" + "?api_key=78d991126ffe36d8e5a86072b85d5d2c&language=es-MX";
     
     const respuesta = await fetch(url);
     const datos = await respuesta.json();
 
-
-    let peliculas = "";
-    let poster = "";
-    let titulo = "";
-
-    for (let i = 0; i < cantidadPeliculas; i++) {
-        let pelicula = datos.results[i];
-        peliculas += `
-            <div class="pelicula">
-                <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
-                <h3 class="titulo">${pelicula.title}</h3>
-            </div>
-            `;
+        for (let i = 0; i < 20; i++) {
+            let pelicula = datos.results[i];
+            let idPelicula = pelicula.id;
+            posterTituloPelicula(idPelicula);
+            videoPelicula(idPelicula);
         };
+};
 
+const posterTituloPelicula = async(idPelicula) => {
+    const div = document.createElement("div");
+    div.className = "poster"
+    const frag = document.createDocumentFragment();
+    try {
+        const url = "https://api.themoviedb.org/3/movie/" + idPelicula + "?api_key=78d991126ffe36d8e5a86072b85d5d2c&language=es-MX";
+            
+        const respuesta = await fetch(url);
+        const datos = await respuesta.json();
 
-        document.getElementById(id).innerHTML = peliculas;
+        const img = document.createElement("img");
+        img.src = "https://image.tmdb.org/t/p/w342/" + datos.poster_path;
 
+        const txt = document.createElement("h3");
+        txt.textContent = datos.title;
+
+        div.append(img);
+        div.append(txt);
+        frag.append(div);
+        contenedor_peliculas.append(frag);
+    } catch (error) {
     };
+};
 
+const videoPelicula = async(idPelicula) => {
+    const div = document.createElement("div");
+    div.className = "video"
+    const url = "https://api.themoviedb.org/3/movie/" + idPelicula + "/videos?api_key=78d991126ffe36d8e5a86072b85d5d2c";
+    try {
+        const respuesta = await fetch(url);
+        const datos = await respuesta.json();
 
-mostrarPeliculas(20, "now_playing", "contenedor_inicio");
+        let ultimo = datos.results.length - 1;
+        const key = datos.results[ultimo].key;
+        const video = document.createElement("iframe");
+        video.src = "https://www.youtube.com/embed/" + key;
+        div.append(video);
+        contenedor_peliculas.append(div);
+    } 
+    catch (error) {
+    };
+};
 
-mostrarPeliculas(3, "popular", "contenedor_peliculas");
-
-mostrarPeliculas(5, "upcoming", "contenedor_proximamente");
+mostrarPeliculas();
+// videoPelicula(507089);
+// posterTituloPelicula(507089);
