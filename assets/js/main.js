630eb1994c032cdd21c301e8f8f396870d8f3a3c
Roberto <btofpach@gmail.com>
2023-11-09 19:49:24 -0300

    corrigiendo error

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,103 @@
-const fila = document.querySelector('.contenedor-carousel');
-const peliculas = document.querySelectorAll('.pelicula');
 
-const flechaIzquierda = document.getElementById('flecha-izquierda');
-const flechaDerecha = document.getElementById('flecha-derecha');
 
-// ? ----- ----- Event Listener para la flecha derecha. ----- -----
-flechaDerecha.addEventListener('click', () => {
-	fila.scrollLeft += fila.offsetWidth;
 
-	const indicadorActivo = document.querySelector('.indicadores .activo');
-	if(indicadorActivo.nextSibling){
-		indicadorActivo.nextSibling.classList.add('activo');
-		indicadorActivo.classList.remove('activo');
-	}
-});
+const appindex = async() => {
 
-// ? ----- ----- Event Listener para la flecha izquierda. ----- -----
-flechaIzquierda.addEventListener('click', () => {
-	fila.scrollLeft -= fila.offsetWidth;
 
-	const indicadorActivo = document.querySelector('.indicadores .activo');
-	if(indicadorActivo.previousSibling){
-		indicadorActivo.previousSibling.classList.add('activo');
-		indicadorActivo.classList.remove('activo');
-	}
-});
+	// Carga peliculas en el div carusel
+    const url = "https://api.themoviedb.org/3/movie/" + "top_rated" + "?api_key=78d991126ffe36d8e5a86072b85d5d2c&language=es-MX";
+     
+    const contenedor = document.querySelector(".carousel");
+    
+    const frag = document.createDocumentFragment();
+    
+    const respuesta = await fetch(url);
+    const datos = await respuesta.json();
 
-// ? ----- ----- Paginacion ----- -----
-const numeroPaginas = Math.ceil(peliculas.length / 5);
-for(let i = 0; i < numeroPaginas; i++){
-	const indicador = document.createElement('button');
+        for (let i = 0; i < 10; i++) {
+            let pelicula = datos.results[i];
+            console.log(pelicula)
 
-	if(i === 0){
-		indicador.classList.add('activo');
-	}
+            const div = document.createElement("div");
+            div.classList.add("pelicula");
 
-	document.querySelector('.indicadores').appendChild(indicador);
-	indicador.addEventListener('click', (e) => {
-		fila.scrollLeft = i * fila.offsetWidth;
+            const img = document.createElement("img");
+            img.src = "https://image.tmdb.org/t/p/w500/" + pelicula.poster_path;
 
-		document.querySelector('.indicadores .activo').classList.remove('activo');
-		e.target.classList.add('activo');
+            const anc = document.createElement("a");
+            anc.href = "#"
+
+            div.append(anc);
+            anc.append(img)
+            frag.append(div)   
+        };
+    contenedor.append(frag);
+
+
+
+	// 			manejar flechas y indicadores
+
+	const fila = document.querySelector('.contenedor-carousel');
+	const peliculas = document.querySelectorAll('.pelicula');
+
+	const flechaIzquierda = document.getElementById('flecha-izquierda');
+	const flechaDerecha = document.getElementById('flecha-derecha');
+
+	// ? ----- ----- Event Listener para la flecha derecha. ----- -----
+	flechaDerecha.addEventListener('click', () => {
+		fila.scrollLeft += fila.offsetWidth;
+
+		const indicadorActivo = document.querySelector('.indicadores .activo');
+		if(indicadorActivo.nextSibling){
+			indicadorActivo.nextSibling.classList.add('activo');
+			indicadorActivo.classList.remove('activo');
+		}
 	});
-}
 
-// ? ----- ----- Hover ----- -----
-peliculas.forEach((pelicula) => {
-	pelicula.addEventListener('mouseenter', (e) => {
-		const elemento = e.currentTarget;
-		setTimeout(() => {
-			peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
-			elemento.classList.add('hover');
-		}, 300);
+	// ? ----- ----- Event Listener para la flecha izquierda. ----- -----
+	flechaIzquierda.addEventListener('click', () => {
+		fila.scrollLeft -= fila.offsetWidth;
+
+		const indicadorActivo = document.querySelector('.indicadores .activo');
+		if(indicadorActivo.previousSibling){
+			indicadorActivo.previousSibling.classList.add('activo');
+			indicadorActivo.classList.remove('activo');
+		}
 	});
-});
 
-fila.addEventListener('mouseleave', () => {
-	peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
-});
+	// ? ----- ----- Paginacion ----- -----
+	const numeroPaginas = Math.ceil(peliculas.length / 5);
+	for(let i = 0; i < numeroPaginas; i++){
+		const indicador = document.createElement('button');
+
+		if(i === 0){
+			indicador.classList.add('activo');
+		}
+
+		document.querySelector('.indicadores').appendChild(indicador);
+		indicador.addEventListener('click', (e) => {
+			fila.scrollLeft = i * fila.offsetWidth;
+
+			document.querySelector('.indicadores .activo').classList.remove('activo');
+			e.target.classList.add('activo');
+		});
+	}
+
+	// ? ----- ----- Hover ----- -----
+	peliculas.forEach((pelicula) => {
+		pelicula.addEventListener('mouseenter', (e) => {
+			const elemento = e.currentTarget;
+			setTimeout(() => {
+				peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
+				elemento.classList.add('hover');
+			}, 300);
+		});
+	});
+
+	fila.addEventListener('mouseleave', () => {
+		peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
+	});
+
+};
+
+appindex();
